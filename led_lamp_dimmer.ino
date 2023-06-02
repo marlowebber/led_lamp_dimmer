4e7154b46c5457b30fc4a318f65b2374b4ffddd9
marlowebber <marlo.m.webber@gmail.com>
2023-06-02 15:56:18 +1000

    m

--- led_lamp_dimmer.ino
+++ led_lamp_dimmer.ino
@@ -10,18 +10,8 @@ int loopCount = 0;
 const int loopCountCheck = 100;
 
 
-void setup() {
-  pinMode(outPin, OUTPUT);   
-  Serial.begin(9600);       
-}
-
-void loop() {
-  digitalWrite(outPin, HIGH);
-  delayMicroseconds(freqUs - dutyOffUs);
-  digitalWrite(outPin, LOW);
-  delayMicroseconds(dutyOffUs);
-  if (loopCount > loopCountCheck)
-  {
+int measure()
+{
     val = analogRead(analogPin); 
 
     float v = val;
@@ -32,9 +22,49 @@ void loop() {
 
     float ndc = ff * r;
 
-    dutyOffUs = ndc;
+    int wemm = ndc;
     
-    Serial.println(dutyOffUs);  
+    Serial.println(wemm);  
+
+    return wemm;
+}
+
+
+void doLights(int dtc)
+{
+  digitalWrite(outPin, HIGH);
+  delayMicroseconds(freqUs - dtc);
+  digitalWrite(outPin, LOW);
+  delayMicroseconds(dtc);
+}
+
+
+void startup()
+{
+  delay(5000); // allow capacitors to charge up fully.
+
+  dutyOffUs = measure();
+  int tempDuty = 0;
+  
+  while(tempDuty < dutyOffUs);
+  {
+    doLights(tempDuty);
+    tempDuty++;
+  }
+}
+
+
+void setup() {
+  pinMode(outPin, OUTPUT);   
+  Serial.begin(9600);       
+  startup();
+}
+
+void loop() {
+  doLights(dutyOffUs);
+  if (loopCount > loopCountCheck)
+  {
+    dutyOffUs = measure();
     loopCount = 0;
   }
   loopCount++;
